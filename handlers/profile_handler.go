4ab4fa2f53ccf20f910ea6e984955d1f3876f85d
Yaroslav Klymovets <yaroslav.klymovets@gmail.com>
2024-06-20 11:40:24 +0300

    handle profile requests

--- handlers/profile_handler.go
+++ handlers/profile_handler.go
@@ -2,13 +2,30 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/myschool-petproj/class-ms/service"
 	"net/http"
 )
 
 func GetProfiles(w http.ResponseWriter, r *http.Request) {
-	_ = json.NewEncoder(w).Encode("All profiles")
+	profiles := service.GetAllProfiles()
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(profiles)
 }
 
 func GetProfile(w http.ResponseWriter, r *http.Request) {
-	_ = json.NewEncoder(w).Encode("Profile")
+	id := chi.URLParam(r, "id")
+	err := uuid.Validate(id)
+	if err != nil {
+		http.Error(w, "id should be uuid", http.StatusBadRequest)
+		return
+	}
+	profile, err := service.GetProfileById(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(profile)
 }
